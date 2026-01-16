a57dd5f9e80aeec5d651e1cee11a75d523e08015
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-01-16 18:50:31 +0800

    try

---
+++ Arcade Day Pass/arcade.py
@@ -0,0 +1,13 @@
+customerName = 'john'
+passes = 3
+total_tokens = 4
+price = 15.1
+requiredTokens = 1
+
+game_available = total_tokens // price
+
+print("Customer name:" + customerName)
+print("Total tokens:" + str(total_tokens))
+print("Price:" + str(price))
+print("Required tokens:" + str(requiredTokens))
+print("Game available:" + str(game_available))
